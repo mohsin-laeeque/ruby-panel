f644f5c31c4cfd1a99f2a26f87e07f28533a26df
Alinawaz-777 <55326140+Alinawaz-786@users.noreply.github.com>
2023-07-19 11:42:07 +0500

    Routes Update

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
         sessions: 'admins/sessions',
         registrations: 'admins/registrations'
   }
+   get '/' => 'suppliers#index'
    resources :suppliers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
