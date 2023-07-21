6bba965fc85a14f4d0808fea27b3045dbd9a1e47
Alinawaz-777 <87475082+AliNawaz-777@users.noreply.github.com>
2023-07-21 18:50:32 +0500

    Document

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -13,5 +13,21 @@ class DocumentsController < ApplicationController
      @action = 'Save'
   end
 
+   def create
+         @document =  Document.new(document_params);
+             if @document.save
+               redirect_to documents_new_path(document_params['article_id']), notice: 'Document has been create successfully'
+             else
+             @action = 'Save'
+             render :new
+          end
+   end
 
+   private def document_params
+             params.require(:document).permit(
+                :name,
+                :doc_type,
+                :article_id,
+             )
+   end
 end
