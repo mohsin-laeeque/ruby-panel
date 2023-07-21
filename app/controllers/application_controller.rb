2104e83a965ca48326b55852c0a49dc9a22a03dd
usman-tahir-arhamsoft <usman.tahir@arhamsoft.org>
2023-07-21 16:33:40 +0500

    some miner changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
     def hello
-        render html: "Hello, World!"
+        render html: "Hello, Ruby! Deployed to Ruby Application"
     end
 end
